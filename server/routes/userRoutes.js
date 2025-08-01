46954ca98503eb98ac559c3e1af221b7155e0c5c
Sabal Adhikari <sadaladhikari@gmail.com>
2025-08-01 15:33:01 +0545

    Update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAdminRoute, protectRoute } from '../middlewares/authMiddleware.js';
-import { getTeamList, loginUser, logoutUser, registerUser } from '../controllers/userController.js';
+import { activateUserProfile, changeUserPassword, deleteUserProfile, getNotificationsList, getTeamList, loginUser, logoutUser, markNotificationRead, registerUser, updateUserProfile } from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -9,14 +9,14 @@ router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
 router.get("/get-team", protectRoute, isAdminRoute, getTeamList);
-// router.get("/notifications", protectRoute, getNotificationsList);
+router.get("/notifications", protectRoute, getNotificationsList);
 
-// router.put("/profile", protectRoute, updateUserProfile);
-// router.put("/read-noti", protectRoute, markNotificationRead);
-// router.put("/change-password", protectRoute, changeUserPassword);
-// //FOR ADMIN ONLY - ADMIN ROUTES
-// router
-// .route("/:id")
-// .put(protectRoute, isAdminRoute, activateUserProfile)
-// .delete(protectRoute, isAdminRoute, deleteUserProfile);
+router.put("/profile", protectRoute, updateUserProfile);
+router.put("/read-noti", protectRoute, markNotificationRead);
+router.put("/change-password", protectRoute, changeUserPassword);
+// ADMIN ROUTES
+router
+.route("/:id")
+.put(protectRoute, isAdminRoute, activateUserProfile)
+    .delete(protectRoute, isAdminRoute, deleteUserProfile);
 export default router;
--- server/utils/index.js
+++ server/utils/index.js
@@ -19,7 +19,7 @@ export const createJWT = (res,userId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
-        sameSite: "strick",
-    maxAge:1*26*60*60*1000,//1day
+        sameSite: "Strict",
+    maxAge:1*24*60*60*1000,//1day
     });
 };
